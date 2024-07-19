78b7f2bcd8d81801ac36e7284630430d2e16d69f
msvalandro <msvalandro@proton.me>
2024-07-19 12:29:25 +0100

    chore: setup styled components and custom fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
+import {
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+  useFonts,
+} from '@expo-google-fonts/nunito-sans'
 import { Text, View } from 'react-native'
+import { ThemeProvider } from 'styled-components/native'
+
+import theme from './src/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold })
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>hello world</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {fontsLoaded ? <Text>hello world</Text> : <Text>Loading...</Text>}
+      </View>
+    </ThemeProvider>
   )
 }
---
+++ src/theme/index.tsx
@@ -0,0 +1,33 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+
+    GRAY_100: '#FAFAFA',
+    GRAY_200: '#EFF0F0',
+    GRAY_300: '#DDDEDF',
+    GRAY_400: '#B9BBBC',
+    GRAY_500: '#5C6265',
+    GRAY_600: '#333638',
+    GRAY_700: '#1B1D1E',
+
+    GREEN_LIGHT: '#E5F0DB',
+    GREEN: '#CBE4B4',
+    GREEN_DARK: '#639339',
+
+    RED_LIGHT: '#F4E6E7',
+    RED: '#F3BABD',
+    RED_DARK: '#BF3B44',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Roboto_400Regular',
+    BOLD: 'Roboto_700Bold',
+  },
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    XXL: 32,
+  },
+}
