d4b7326acc06c90be4c44994544964dfba0c2302
msvalandro <msvalandro@proton.me>
2024-07-31 16:08:43 +0100

    feat: add validation to input fields

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@components/Button'
 import { RadioGroup } from '@components/RadioGroup'
 import { Row } from '@components/Row'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { useNavigation } from '@react-navigation/native'
 import { Controller, useForm } from 'react-hook-form'
+import * as yup from 'yup'
 
 import {
   BackIcon,
@@ -24,8 +26,24 @@ interface FormDataProps {
   valid: boolean
 }
 
+const registerMealSchema = yup.object({
+  name: yup.string().required(),
+  description: yup.string().required(),
+  date: yup
+    .string()
+    .matches(/\b(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/([0-9]{4})\b/)
+    .required(),
+  hour: yup
+    .string()
+    .matches(/\b([01][0-9]|2[0-3]):([0-5][0-9])\b/)
+    .required(),
+  valid: yup.boolean().required(),
+})
+
 export function Register() {
-  const { control, handleSubmit } = useForm<FormDataProps>()
+  const { control, handleSubmit } = useForm<FormDataProps>({
+    resolver: yupResolver(registerMealSchema),
+  })
 
   const navigation = useNavigation()
 
@@ -55,7 +73,11 @@ export function Register() {
             control={control}
             name="name"
             render={({ field: { value, onChange } }) => (
-              <Input value={value} onChangeText={onChange} />
+              <Input
+                placeholder="ex: Acarajé"
+                value={value}
+                onChangeText={onChange}
+              />
             )}
           />
         </InputContainer>
@@ -66,7 +88,11 @@ export function Register() {
             control={control}
             name="description"
             render={({ field: { value, onChange } }) => (
-              <Input value={value} onChangeText={onChange} />
+              <Input
+                placeholder="ex: Bolinho de acarajé na praia de Salvador"
+                value={value}
+                onChangeText={onChange}
+              />
             )}
           />
         </InputContainer>
@@ -78,7 +104,11 @@ export function Register() {
               control={control}
               name="date"
               render={({ field: { value, onChange } }) => (
-                <Input value={value} onChangeText={onChange} />
+                <Input
+                  placeholder="ex: 17/12/2024"
+                  value={value}
+                  onChangeText={onChange}
+                />
               )}
             />
           </InputContainer>
@@ -89,7 +119,11 @@ export function Register() {
               control={control}
               name="hour"
               render={({ field: { value, onChange } }) => (
-                <Input value={value} onChangeText={onChange} />
+                <Input
+                  placeholder="ex: 16:20"
+                  value={value}
+                  onChangeText={onChange}
+                />
               )}
             />
           </InputContainer>
